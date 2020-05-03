7d0872ecba5d79f13d6a25d7569d80bdcde767d0
Subhajit DasGupta <subhajit.dasgupta@yahoo.com>
2020-05-03 09:13:50 -0500

    Updated install-go.sh with list of tools needed to support IDE's.

--- install-go.sh
+++ install-go.sh
@@ -11,6 +11,24 @@ echo "export GOROOT=/usr/local/go" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.bashrc
 echo "Please source ~/.profile"
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+go get -u github.com/stamblerre/gocode
+go get -u github.com/ramya-rao-a/go-outline
+go get -u github.com/newhook/go-symbols
+go get -u github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u golang.org/x/tools/cmd/gorename
+gp get -u golang.org/x/tools/cmd/guru
+go get -u github.com/sqs/goreturns
+go get golang.org/x/tools/cmd/goimports
+go get -u github.com/zmb3/gogetdoc
+go get -u github.com/mgechev/revive
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/haya14busa/goplay
+go get -u github.com/josharian/impl
+go get -u github.com/cweill/gotests
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
 #sudo apt install golang-golang-x-tools
 #go get -u -v github.com/nsf/gocode
 #go get -u -v golang.org/x/tools/cmd/guru
