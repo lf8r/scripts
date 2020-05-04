a9056aca546382c13037ff6d46cc9ce8237993e3
Subhajit DasGupta <subhajit.dasgupta@hpe.com>
2020-05-04 05:16:28 +0000

    Updated script.

--- install/install-dev.sh
+++ install/install-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt -y install gcc make perl                                                                                       sudo apt-get install build-essential                                                                                    sudo apt install zlib1g-dev                                                                                             sudo apt install zlib1g
 sudo apt install openjdk-8-jdk
-
+sudo snap install code --classic
