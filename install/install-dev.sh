0ff86bc5a91fb87a0d2447007f92c8cbc8c7f05a
Subhajit DasGupta <subhajit.dasgupta@hpe.com>
2020-05-04 05:39:22 +0000

    Update script.

--- install/install-dev.sh
+++ install/install-dev.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+sudo apt install git
+sudo apt -y install postgresql-client
+sudo snap install code --classic
+git config --global core.editor "code --wait"
 sudo apt -y install gcc make perl                                                                                       sudo apt-get install build-essential                                                                                    sudo apt install zlib1g-dev                                                                                             sudo apt install zlib1g
 sudo apt install openjdk-8-jdk
-sudo snap install code --classic
+echo "alias vi=\"code --wait\"" >> $HOME/.bashrc
+sudo apt install protobuf-compiler
