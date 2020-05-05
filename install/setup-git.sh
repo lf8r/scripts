45098fb91bbd9bdb86dbfbae4d25a939c90818a2
Subhajit DasGupta <subhajit_dasgupta@yahoo.com>
2020-05-05 12:29:08 +0000

    Created a "setup-git.sh" file to configure git.

---
+++ install/setup-git.sh
@@ -0,0 +1,4 @@
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+git config --global user.email "subhajit_dasgupta@yahoo.com"
+git config --global user.name "Subhajit DasGupta"
+git config --global core.editor leafpad
