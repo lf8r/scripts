c24ea13ffc52138ea271af1bcdfafd03ff7c7696
Subhajit DasGupta <subhajit.dasgupta@hpe.com>
2020-05-25 08:14:00 -0500

    Changed clone script to reflect new github username.

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone git@github.com:subhajitdasgupta/$1.git
+git clone git@github.com:lf8r/$1.git
 cd $1
 git submodule update --init --recursive
 git submodule --quiet foreach --recursive git checkout master
