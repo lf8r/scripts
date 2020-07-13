2ef26127577a9f708b5e2074766a47ffd03001ec
Subhajit DasGupta <subhajit.dasgupta@hpe.com>
2020-07-13 14:10:59 -0500

    Fixed get-current-upstreamsh script

--- get-current-upstream.sh
+++ get-current-upstream.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-git branch -vv | grep `cb` | cut -d " " -f 4 | cut -d "/" -f 1 | cut -d "[" -f 2
+#!/bin/bash
+
+cb=`git rev-parse --abbrev-ref HEAD`
+export test=`git branch -vv | grep $cb`
+#echo $test
+IFS=' ' read -a arr <<< $test
+test1=${arr[3]}
+IFS='/' read -a arr1 <<< $test1
+test2=${arr1[0]}
+IFS='[' read -a arr2 <<< $test2
+echo ${arr2[1]}
