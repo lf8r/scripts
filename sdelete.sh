c62fc1bfc9cdd2686c7a1bf7125e2089f2d599d5
Subhajit dasGupta <subhajit_dasgupta@yahoo.com>
2020-09-05 06:24:08 -0500

    Updates.

--- sdelete.sh
+++ sdelete.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+rm -rf $HOME/.lemminx
+rm -rf $HOME/.cache/go-build
 docker system prune --all --force
 #sudo /etc/init.d/docker stop
 sudo journalctl --vacuum-time=10d
-dd if=/dev/zero of=zerofillfile bs=1M
-rm zerofillfile
+#dd if=/dev/zero of=zerofillfile bs=1M
+#rm zerofillfile
 
